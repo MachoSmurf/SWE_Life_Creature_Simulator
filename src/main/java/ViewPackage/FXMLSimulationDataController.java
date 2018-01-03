b10c9144a345f4d9589b5d7abd04d859ddb3891f
nataschazorg <nataschazorg@outlook.com>
2018-01-03 19:48:54 +0100

    Laatste versie 03-01-2018 19:45 uur.

---
+++ src/main/java/ViewPackage/FXMLSimulationDataController.java
@@ -0,0 +1,82 @@
+package ViewPackage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML Controller class
+ *
+ * @author Natascha Zorg-Wijnhoven
+ */
+public class FXMLSimulationDataController extends UIController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void onShowSimulationDataClick(ActionEvent actionEvent) {
+    }
+
+    public void onCancelClick(ActionEvent actionEvent) {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
